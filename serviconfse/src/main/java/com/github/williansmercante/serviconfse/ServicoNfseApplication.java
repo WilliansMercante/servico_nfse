f9a974b03ba06e0bc8b318c95feabab32c231664
WilliansMercante <willians_mercante@hotmail.com>
2025-09-06 15:10:17 -0300

    adicionando o projeto

---
+++ serviconfse/src/main/java/com/github/williansmercante/serviconfse/ServicoNfseApplication.java
@@ -0,0 +1,13 @@
+package com.github.williansmercante.serviconfse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoNfseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoNfseApplication.class, args);
+	}
+
+}
---
+++ serviconfse/src/test/java/com/github/williansmercante/serviconfse/ServicoNfseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.williansmercante.serviconfse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoNfseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
